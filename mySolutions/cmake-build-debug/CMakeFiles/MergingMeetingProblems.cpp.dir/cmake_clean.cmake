5d04a8ceec7c7e5efb9b6ac8992ec6764f30d2c3
commandantekaustav <kaustavbasu97@gmail.com>
2020-11-08 20:29:43 +0530

    pdf + lest + soln
    1) Added pdf notes from different lectures
    
    2) There were some issues regarding "lest.hpp" which is a c++11  native  library for testing purposes. I have to add this so that these codes become properly adaptable with interviewcake environment.
    
    3) Added solution for Merged Meeting Problem. (lest.hpp tests are working this time.)

---
+++ mySolutions/cmake-build-debug/CMakeFiles/MergingMeetingProblems.cpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MergingMeetingProblems.cpp.dir/MergingMeetingProblems.cpp.obj"
+  "MergingMeetingProblems.cpp.exe"
+  "MergingMeetingProblems.cpp.exe.manifest"
+  "MergingMeetingProblems.cpp.pdb"
+  "libMergingMeetingProblems.cpp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MergingMeetingProblems.cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ mySolutions/cmake-build-debug/CMakeFiles/mySolutions.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mySolutions.dir/main.cpp.obj"
+  "libmySolutions.dll.a"
+  "mySolutions.exe"
+  "mySolutions.exe.manifest"
+  "mySolutions.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mySolutions.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
